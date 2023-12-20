4ae5c3eff10f59b608fa96172d1600515ac11856
Liam Cottrell <lemcottrell@gmail.com>
2023-12-20 18:13:31 +0000

    build currency queryURL

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,38 +1,39 @@
 let currencies = $("#currencies");
+
 let currencyList = [
-    "EUR",
-    "USD",
-    "JPY",
-    "BGN",
-    "CZK",
-    "DKK",
-    "GBP",
-    "HUF",
-    "PLN",
-    "RON",
-    "SEK",
-    "CHF",
-    "ISK",
-    "NOK",
-    "HRK",
-    "RUB",
-    "TRY",
-    "AUD",
-    "BRL",
-    "CAD",
-    "CNY",
-    "HKD",
-    "IDR",
-    "ILS",
-    "INR",
-    "KRW",
-    "MXN",
-    "MYR",
-    "NZD",
-    "PHP",
-    "SGD",
-    "THB",
-    "ZAR",
+    "EUR - Euro",
+    "USD - US Dollar",
+    "JPY - Japanese Yen",
+    "BGN - Bulgarian Lev",
+    "CZK - Czech Republic Koruna",
+    "DKK - Danish Krone",
+    "GBP - British Pound Sterling",
+    "HUF - Hungarian Forint",
+    "PLN - Polish Zloty",
+    "RON - Romanian Leu",
+    "SEK - Swedish Krona",
+    "CHF - Swiss Franc",
+    "ISK - Icelandic Króna",
+    "NOK - Norwegian Krone",
+    "HRK - Croatian Kuna",
+    "RUB - Russian Ruble",
+    "TRY - Turkish Lira",
+    "AUD - Australian Dollar",
+    "BRL - Brazilian Real",
+    "CAD - Canadian Dollar",
+    "CNY - Chinese Yuan",
+    "HKD - Hong Kong Dollar",
+    "IDR - Indonesian Rupiah",
+    "ILS - Israeli New Sheqel",
+    "INR - Indian Rupee",
+    "KRW - South Korean Won",
+    "MXN - Mexican Peso",
+    "MYR - Malaysian Ringgit",
+    "NZD - New Zealand Dollar",
+    "PHP - Philippine Peso",
+    "SGD - Singapore Dollar",
+    "THB - Thai Baht",
+    "ZAR - South African Rand"
     ];
 
 // Make currency dropdown
@@ -42,25 +43,24 @@ for (let i = 0; i < currencyList.length; i++) {
 }
 
 // Fetch currency data from API
-function fetchCurrency (country) {
-    let queryURLCurrency = "https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_NOCDhLaiS0pA01mLhYHikP55sb2tvwMFcFZ4m0nc&currencies=EUR%2CUSD%2CCAD&base_currency=GBP";
+function fetchCurrency (chosenCurrency) {
+    let queryURLCurrency = "https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_NOCDhLaiS0pA01mLhYHikP55sb2tvwMFcFZ4m0nc&currencies=" + chosenCurrency + "&base_currency=" + chosenCurrency;
     fetch(queryURLCurrency)
         .then(function(responseCurrency){
         return responseCurrency.json();
     }).then(function(dataCurrency){
         console.log(dataCurrency);
 });
-
 };
 
 // Run functions when form button is clicked
 $("#curSubmit").on("click", function(event) {
     event.preventDefault();
-    let country = $("#currencies").val();
-    console.log(country);
-    // fetchCurrency(country);
+    let chosenCurrency = $("#currencies").val();
+    chosenCurrency = chosenCurrency.substring(0,3);
+    console.log(chosenCurrency);
+    fetchCurrency(chosenCurrency);
 });
 
 // Make card with API info
-
 
