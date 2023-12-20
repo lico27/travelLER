6b140df72625c4efdd8df1f6aaf6cdce689ce955
Liam Cottrell <lemcottrell@gmail.com>
2023-12-20 17:09:12 +0000

    make currency selection forloop

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,48 @@
+let currencies = $("#currencies");
+let currencyList = [
+    "EUR",
+    "USD",
+    "JPY",
+    "BGN",
+    "CZK",
+    "DKK",
+    "GBP",
+    "HUF",
+    "PLN",
+    "RON",
+    "SEK",
+    "CHF",
+    "ISK",
+    "NOK",
+    "HRK",
+    "RUB",
+    "TRY",
+    "AUD",
+    "BRL",
+    "CAD",
+    "CNY",
+    "HKD",
+    "IDR",
+    "ILS",
+    "INR",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NZD",
+    "PHP",
+    "SGD",
+    "THB",
+    "ZAR",
+    ];
+
+// Make currency dropdown
+for (let i = 0; i < currencyList.length; i++) {
+    let eachCurrency = currencyList[i];
+    currencies.append("<option>" + eachCurrency + "</option").attr("value", eachCurrency);
+}
+
 // Fetch currency data from API
-function fetchCurrency () {
+function fetchCurrency (country) {
     let queryURLCurrency = "https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_NOCDhLaiS0pA01mLhYHikP55sb2tvwMFcFZ4m0nc&currencies=EUR%2CUSD%2CCAD&base_currency=GBP";
     fetch(queryURLCurrency)
         .then(function(responseCurrency){
@@ -11,10 +54,13 @@ function fetchCurrency () {
 };
 
 // Run functions when form button is clicked
-$("#btnTest").on("click", function(event) {
+$("#curSubmit").on("click", function(event) {
     event.preventDefault();
-    let searchInput = $("#search-input").val();
-    console.log(searchInput);
-    fetchCurrency();
+    let country = $("#currencies").val();
+    console.log(country);
+    // fetchCurrency(country);
 });
+
+// Make card with API info
+
 
